01d363de2ede33fd50d028aa2e7d53e662c41165
road-pig <qiuzeeming@gmail.com>
2021-04-17 21:00:50 +0800

    i forgot to commit this

--- AndroidInputDriver/MainPage.xaml.cs
+++ AndroidInputDriver/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
 using Windows.Networking.Sockets;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
@@ -23,9 +24,14 @@ namespace AndroidInputDriver {
     /// </summary>
     public sealed partial class MainPage : Page {
         private static string SERVER_PORT = "3939";
+        private VirtualDevice virtualDevice;
+        private uint width;
+        private uint height;
         public MainPage() {
             this.InitializeComponent();
-            startServer();
+            connectButton.AddHandler(PointerPressedEvent, new PointerEventHandler(Connect), true);
+            width = DisplayInformation.GetForCurrentView().ScreenWidthInRawPixels;
+            height = DisplayInformation.GetForCurrentView().ScreenHeightInRawPixels;
         }
 
         private async void startServer() {
@@ -42,13 +48,67 @@ namespace AndroidInputDriver {
                 this.serverListBox.Items.Add(socketErrorStatus.ToString() != "Unknown" ? socketErrorStatus.ToString() : e.Message);
             }
         }
-        public async void StreamSocketListener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args) {
-            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add("connection received"));
+        private async void StreamSocketListener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args) {
+            this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add("connection received"));
             StreamReader streamReader = new StreamReader(args.Socket.InputStream.AsStreamForRead());
+            string handshake;
+            handshake = await streamReader.ReadLineAsync();
+            if (handshake.Equals("Android Input Device")) {
+                this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add("handshake message received"));
+                using (Stream outputStream = args.Socket.OutputStream.AsStreamForWrite()) {
+                    using (var streamWriter = new StreamWriter(outputStream)) {
+                        await streamWriter.WriteLineAsync("Android Input Driver");
+                        await streamWriter.FlushAsync();
+                    }
+                }
+            } else {
+                sender.Dispose();
+                return;
+            }
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add("connection received"));
             for (; ; ) {
                 string request;
                 request = await streamReader.ReadLineAsync();
-                await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add(string.Format("server received the request: \"{0}\"", request)));
+                if(request == null) {
+                    sender.Dispose();
+                    return;
+                }
+                this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add(string.Format("server received the request: \"{0}\"", request)));
+                InjectInput(request);
+            }
+        }
+        private void Connect(object sender, PointerRoutedEventArgs e) {
+            startServer();
+            virtualDevice = new VirtualDevice(e.GetCurrentPoint((Button) sender));
+            virtualDevice.InjectInputForPen(1000, 1000, 0, false);
+        }
+        private async void InjectInput(String request) {
+            string[] split = request.Split(':');
+            bool contact;
+            if (split[0].Equals("DOWN")) {
+                contact = true;
+                int x, y;
+                double pressure;
+                string[] split2 = split[1].Split(',');
+                x = (int)(Double.Parse(split2[0]) * width);
+                y = (int)(Double.Parse(split2[1]) * height);
+                pressure = Double.Parse(split2[2]);
+                virtualDevice.InjectInputForPen(x, y, pressure, contact);
+            } else if(split[0].Equals("HOVER")) {
+                contact = false;
+                int x, y;
+                double pressure;
+                string[] split2 = split[1].Split(',');
+                x = (int)(Double.Parse(split2[0]) * width);
+                y = (int)(Double.Parse(split2[1]) * height);
+                pressure = Double.Parse(split2[2]);
+                virtualDevice.InjectInputForPen(x, y, pressure, contact);
+            }else if (split[0].Equals("MACRO")) {
+
+                await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add(split[1]));
+                if (!String.IsNullOrWhiteSpace(split[1])) {
+                    virtualDevice.InjectInputForKeyboard(split[1].Split(","));
+                }
             }
         }
     }
---
+++ AndroidInputDriver/VirtualDevice.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Input;
+using Windows.UI.Input.Preview.Injection;
+
+namespace AndroidInputDriver {
+    class VirtualDevice {
+
+        private InputInjector inputInjector;
+        private uint pointerID;
+        bool prevContact = false;
+
+        public VirtualDevice(PointerPoint point) {
+            inputInjector = InputInjector.TryCreate();
+
+            pointerID = point.PointerId + 1;
+        }
+        public void InjectInputForPen(int x, int y, double pressure, bool contact) {
+            prevContact = contact;
+            InjectedInputPenInfo info = new InjectedInputPenInfo();
+            info.PenParameters = InjectedInputPenParameters.Pressure;
+            InjectedInputPointerOptions options;
+            if (contact) {
+                if (prevContact) {
+                    options = InjectedInputPointerOptions.Update | InjectedInputPointerOptions.InContact | InjectedInputPointerOptions.InRange;
+                } else {
+                    options = InjectedInputPointerOptions.PointerDown | InjectedInputPointerOptions.InContact | InjectedInputPointerOptions.InRange;
+                    System.Diagnostics.Debug.WriteLine("pointer down");
+                }
+            } else {
+                if (prevContact) {
+                    options = InjectedInputPointerOptions.PointerUp | InjectedInputPointerOptions.InRange;
+                } else {
+                    options = InjectedInputPointerOptions.Update | InjectedInputPointerOptions.InRange;
+                }
+            }
+            info.PointerInfo = new InjectedInputPointerInfo {
+                PointerId = pointerID,
+                PointerOptions = options,
+                TimeOffsetInMilliseconds = 0,
+                PixelLocation = new InjectedInputPoint {
+                    PositionX = x,
+                    PositionY = y
+                }
+            };
+            info.Pressure = pressure;
+            inputInjector.InjectPenInput(info);
+        }
+        public void InjectInputForKeyboard(string[] codesString) {
+            InjectedInputKeyboardInfo[] info = new InjectedInputKeyboardInfo[codesString.Length];
+            for (int i = 0; i < codesString.Length; i++) {
+                info[i] = new InjectedInputKeyboardInfo();
+                info[i].VirtualKey = ushort.Parse(codesString[i]);
+                info[i].KeyOptions = InjectedInputKeyOptions.None;
+            }
+            inputInjector.InjectKeyboardInput(info);
+            for(int i = 0; i < info.Length; i++) {
+                info[i].KeyOptions = InjectedInputKeyOptions.KeyUp;
+            }
+            inputInjector.InjectKeyboardInput(info);
+        }
+    }
+}
